79918a068ac37f15f08e2e3b95b096a6093ed252
jow76 <joejardine76@gmail.com>
2023-07-26 15:56:10 +0100

    added compatibility with front-end

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get("/", (request, response)=>{
 })
 
 app.get("/photos", async (request, response) =>{
-    const API = `https://api.unsplash.com/search/photos/?client_id=${process.env.ACCESS_KEY}&query=jow`
+    const query = request.query.query
+    const API = `https://api.unsplash.com/search/photos/?client_id=${process.env.ACCESS_KEY}&query=${query}`
     const res = await axios.get(API)
     // console.log(res.data.results[0].urls.regular)
     // response.status(200).json("Peaber")
